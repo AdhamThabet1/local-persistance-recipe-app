d711e35762aecdc415f22ac492271f2126820350
AdhamThabet1 <93025667+AdhamThabet1@users.noreply.github.com>
2021-10-23 11:02:42 +0200

    My initial version of favorites view controller

---
+++ FavoritesViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FavoritesViewController.swift
+//  My Recipe
+//
+//  Created by Adham Thabet on 10/10/2021.
+//
+
+import UIKit
+
+class FavoritesViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
